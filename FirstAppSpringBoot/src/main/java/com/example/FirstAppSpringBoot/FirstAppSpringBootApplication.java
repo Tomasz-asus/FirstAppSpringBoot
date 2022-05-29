4f845cc4e338fadb633e0aca0fa1ca3064a00a09
Tomasz G <biuro@forplastics.pl>
2022-05-29 19:32:15 +0200

    Initial commit FirstAppSpringBoot

---
+++ FirstAppSpringBoot/src/main/java/com/example/FirstAppSpringBoot/FirstAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstAppSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppSpringBootApplication.class, args);
+	}
+
+}
